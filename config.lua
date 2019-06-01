e678c40d6db1ded83dcf08bdc1174bf7a536bbd9
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2019-06-01 12:01:00 -0400

    added note +more
    added a tiny note on top of main.lua
    made a config file to add stuff

---
+++ config.lua
@@ -0,0 +1,31 @@
+--[[ 
+
+    dotlauncher is a game inspired by Space Invaders. It is not made to recreate but simulate an experience similar to it.
+    It was made for me (SamuraiStacks) to learn Lua, Love2D and what it's like to actually develop a game from scratch.
+
+    dotlauncher © SamuraiStacks 2019
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+    You are NOT allowed to:
+    i. Copy any of this code and use it in your own projects without full credit.
+    ii. Distribute a modified version of this application claiming it is entirely your work.
+--]]
+
+version = "0.0.39"
